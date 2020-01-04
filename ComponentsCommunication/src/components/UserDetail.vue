fb353d9b9085dafbb4e5a3b4f992b20a7a9ade28
u.ashikov <u.ashikov@abv.bg>
2020-01-04 23:19:47 +0200

    Components communication - communication from parent to child element.

---
+++ ComponentsCommunication/src/components/UserDetail.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>My Name is: {{ userName }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            userName: {
+                type: String,
+                required: true,
+                default: 'Yulian'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
