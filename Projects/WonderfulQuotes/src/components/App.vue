c17676c64aee502f46d2b91a9be31b0e050aa865
u.ashikov <u.ashikov@abv.bg>
2020-01-11 20:50:47 +0200

    Added progress bar, displaying the currently added quotes count.

--- Projects/WonderfulQuotes/src/components/App.vue
+++ Projects/WonderfulQuotes/src/components/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app" class="container-fluid">
-    <h1>Welcome!</h1>
-    <h3>Currently added quotes: {{ quotesCount }}</h3>
-
-    <div class="alert alert-danger" v-if="quotesCount + 1 > maxQuotesCount" role="alert">
+    <div class="alert alert-danger my-3" v-if="quotesCount + 1 > maxQuotesCount" role="alert">
       You have reached the max quotes capacity. Please delete some of them in order to be able to add new.
+    </div>
+
+    <h3 class="my-3">Quotes Count</h3>
+
+    <div class="progress mb-3">
+      <div class="progress-bar" role="progressbar" v-bind:style="{ 'width': quotesCount * maxQuotesCount + '%' }" v-bind="progressBarAttributes" aria-valuemin="0">{{ quotesCount }} / {{ maxQuotesCount }}</div>
     </div>
 
     <form class="col-md-4 mx-auto">
@@ -12,7 +15,7 @@
       <textarea id="quote-text" class="form-control" rows="3">
       </textarea>
 
-      <button id="add-quote-btn" class="btn btn-success my-3 d-block mx-auto" v-on:click="saveQuote($event)">Add Quote</button>
+      <button id="add-quote-btn" class="btn btn-info my-3 d-block mx-auto" v-on:click="saveQuote($event)">Add Quote</button>
     </form>
 
     <app-quotes-deck v-bind:quotes="quotes">
@@ -39,6 +42,14 @@ export default {
       quotesCount: 0,
       maxQuotesCount: 10,
       quotes: []
+    }
+  },
+  computed: {
+    progressBarAttributes: function () {
+      return {
+        'aria-valuenow': this.quotesCount * 10,
+        'aria-valuemax': this.maxQuotesCount * 10
+      }
     }
   },
   methods: {
