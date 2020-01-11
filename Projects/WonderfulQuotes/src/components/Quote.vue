654b9ab473e4f19c6e26dc1f340dc49d89890c44
u.ashikov <u.ashikov@abv.bg>
2020-01-11 19:37:08 +0200

    Created app components - quote and quote deck.

---
+++ Projects/WonderfulQuotes/src/components/Quote.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="quote card-body border rounded col-md-2">
+        {{ quoteText }} {{ index }}!
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['index', 'quoteText']
+}
+</script>
---
+++ Projects/WonderfulQuotes/src/components/QuotesDeck.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <app-quote v-for="(quote, i) in quotes" v-bind:quoteText="quote" v-bind:index="i"></app-quote>
+    </div>
+</template>
+
+<script>
+import Quote from '../components/Quote'
+
+export default {
+    components: {
+        'app-quote': Quote
+    },
+    props: ['quotes']
+}
+</script>
