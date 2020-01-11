116fb3e771f571c80c40c0fc4a1bc34a89cc8be7
u.ashikov <u.ashikov@abv.bg>
2020-01-11 21:38:10 +0200

    Implemented quotes remove functionality. Code refactoring.

--- Projects/WonderfulQuotes/src/components/App.vue
+++ Projects/WonderfulQuotes/src/components/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app" class="container-fluid">
-    <div class="alert alert-danger my-3" v-if="quotesCount + 1 > maxQuotesCount" role="alert">
+    <div class="alert alert-danger my-3" v-if="quotesLimitReached" role="alert">
       You have reached the max quotes capacity. Please delete some of them in order to be able to add new.
     </div>
 
     <h3 class="my-3">Quotes Count</h3>
 
     <div class="progress mb-3">
-      <div class="progress-bar" role="progressbar" v-bind:style="{ 'width': quotesCount * maxQuotesCount + '%' }" v-bind="progressBarAttributes" aria-valuemin="0">{{ quotesCount }} / {{ maxQuotesCount }}</div>
+      <div class="progress-bar" role="progressbar" v-bind:style="{ 'width': quotesCount * maxQuotesCount + '%' }" v-bind="progressBarAttributes" aria-valuemin="0">
+        {{ quotesCount }} / {{ maxQuotesCount }}
+      </div>
     </div>
 
     <form class="col-md-4 mx-auto">
       <h5>Quote</h5>
-      <textarea id="quote-text" class="form-control" rows="3">
-      </textarea>
+      <textarea id="quote-text" class="form-control" rows="3"></textarea>
 
       <button id="add-quote-btn" class="btn btn-info my-3 d-block mx-auto" v-on:click="saveQuote($event)">Add Quote</button>
     </form>
@@ -50,7 +51,18 @@ export default {
         'aria-valuenow': this.quotesCount * 10,
         'aria-valuemax': this.maxQuotesCount * 10
       }
+    },
+    quotesLimitReached: function () {
+      return this.quotesCount + 1 > this.maxQuotesCount;
     }
+  },
+  mounted: function () {
+    eventBus.$on('quoteRemoved', (index) => {
+      if (index >= 0 && index < this.quotes.length) {
+        this.quotes.splice(index, 1);
+        this.quotesCount--;
+      }
+    });
   },
   methods: {
     saveQuote: function (event) {
--- Projects/WonderfulQuotes/src/components/Quote.vue
+++ Projects/WonderfulQuotes/src/components/Quote.vue
@@ -1,15 +1,36 @@
 <template>
-    <div class="col mb-4">
+    <div class="quote col mb-4" v-on:click="removeQuote()">
         <div class="card h-100">
             <div class="card-body">
-                {{ quoteText }} {{ index }}!
+                {{ quoteText }}
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import { eventBus } from '../main'
+
 export default {
-    props: ['index', 'quoteText']
+    props: ['index', 'quoteText'],
+    methods: {
+        removeQuote: function() {
+            eventBus.$emit('quoteRemoved', this.index);
+        }
+    }
 }
 </script>
+
+<style scoped>
+    @import url('https://fonts.googleapis.com/css?family=Dancing+Script&display=swap');
+
+    div.quote {
+        cursor: pointer;
+        font-family: 'Dancing Script', cursive;
+        font-size: 1.6em;
+    }
+
+    div.quote div:hover {
+        background-color: #ffb3b3;
+    }
+</style>
