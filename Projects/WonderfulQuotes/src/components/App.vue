e8e41e7e9dd99eab503d100ac340bc76d919d6e3
u.ashikov <u.ashikov@abv.bg>
2020-01-11 20:21:31 +0200

    Implemented the logic for adding new quotes.

--- Projects/WonderfulQuotes/src/components/App.vue
+++ Projects/WonderfulQuotes/src/components/App.vue
@@ -3,12 +3,24 @@
     <h1>Welcome!</h1>
     <h3>Currently added quotes: {{ quotesCount }}</h3>
 
-    <div class="alert alert-primary" role="alert">
-      Info: Click on a Quote to delete it. 
+    <div class="alert alert-danger" v-if="quotesCount + 1 > maxQuotesCount" role="alert">
+      You have reached the max quotes capacity. Please delete some of them in order to be able to add new.
     </div>
+
+    <form class="col-md-4 mx-auto">
+      <h5>Quote</h5>
+      <textarea id="quote-text" class="form-control" rows="3">
+      </textarea>
+
+      <button id="add-quote-btn" class="btn btn-success my-3 d-block mx-auto" v-on:click="saveQuote($event)">Add Quote</button>
+    </form>
 
     <app-quotes-deck v-bind:quotes="quotes">
     </app-quotes-deck>
+
+    <div class="alert alert-primary" role="alert">
+      Info: Click on a Quote to delete it. 
+    </div>
   </div>
 </template>
 
@@ -25,7 +37,21 @@ export default {
   data: function () {
     return {
       quotesCount: 0,
-      quotes: ['hello there', 'Test']
+      maxQuotesCount: 10,
+      quotes: []
+    }
+  },
+  methods: {
+    saveQuote: function (event) {
+      event.preventDefault();
+
+      if (this.quotesCount != this.maxQuotesCount) {
+        var quoteText = $('#quote-text').val();
+        $('#quote-text').val('');
+
+        this.quotes.push(quoteText);
+        this.quotesCount++;
+      }
     }
   }
 }
