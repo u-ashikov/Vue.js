b8bfbe8a4afde6bcc636e8efffddf7f32417f027
u.ashikov <u.ashikov@abv.bg>
2020-01-12 15:26:50 +0200

    Creating and using custom directives.

---
+++ CustomDirectives/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+
+                <button class="btn btn-info" v-myOn:click="sayHello">Check Directive</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            sayHello: function () {
+                alert('Hi there!');
+            }
+        },
+        directives: {
+            myOn: {
+                bind: function (el, binding, vnode) {
+                    var eventName = binding.arg;
+                    var fnToExecute = binding.value;
+
+                    el.addEventListener(eventName, fnToExecute);
+                }   
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
