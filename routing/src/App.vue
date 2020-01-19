2308bdd4ec13208493a2c0201fd8459d177169c4
u.ashikov <u.ashikov@abv.bg>
2020-01-19 14:05:01 +0200

    Added Header component with navigation links.

--- routing/src/App.vue
+++ routing/src/App.vue
@@ -3,6 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
+                <app-header></app-header>
                 <router-view></router-view>
             </div>
         </div>
@@ -10,7 +11,12 @@
 </template>
 
 <script>
+    import Header from './components/Header.vue'
+
     export default {
+        components: {
+            'app-header': Header
+        }
     }
 </script>
 
---
+++ routing/src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <ul class="nav nav-pills">
+            <router-link to="/">Home</router-link>
+            <router-link to="/user">User</router-link>
+        </ul>
+    </div>
+</template>
