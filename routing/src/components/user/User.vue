56f875e74842277a2311689755e6c73aabe492b4
u.ashikov <u.ashikov@abv.bg>
2020-01-19 14:26:12 +0200

    Navigating programmatically to another route.

--- routing/src/components/user/User.vue
+++ routing/src/components/user/User.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
         <h1>The User Page</h1>
+        <hr>
+        <button class="btn btn-primary" v-on:click="navigateToHome()">Go To Home</button>
     </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        navigateToHome: function () {
+            this.$router.push('/');
+        }
+    }
+}
+</script>
