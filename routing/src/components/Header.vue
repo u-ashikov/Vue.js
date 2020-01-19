3675f8628b1b659e233fbcdd308db0ce726f68fe
u.ashikov <u.ashikov@abv.bg>
2020-01-19 14:44:12 +0200

    Using dynamic route matching with parameters.

--- routing/src/components/Header.vue
+++ routing/src/components/Header.vue
@@ -2,7 +2,8 @@
     <div>
         <ul class="nav nav-pills">
             <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
-            <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
+            <router-link to="/user/1" tag="li" active-class="active"><a>User 1</a></router-link>
+            <router-link to="/user/2" tag="li" active-class="active"><a>User 2</a></router-link>
         </ul>
     </div>
 </template>
--- routing/src/components/user/User.vue
+++ routing/src/components/user/User.vue
@@ -1,17 +1,28 @@
 <template>
-    <div>
-        <h1>The User Page</h1>
-        <hr>
-        <button class="btn btn-primary" v-on:click="navigateToHome()">Go To Home</button>
-    </div>
+  <div>
+    <h1>The User Page</h1>
+    <hr />
+    <p>User with id: {{ id }} has been rendered.</p>
+    <button class="btn btn-primary" v-on:click="navigateToHome()">Go To Home</button>
+  </div>
 </template>
 
 <script>
 export default {
-    methods: {
-        navigateToHome: function () {
-            this.$router.push('/');
-        }
+  data: function() {
+    return {
+      id: this.$route.params.id
+    };
+  },
+  watch: {
+    $route: function(to, from) {
+      this.id = to.params.id;
     }
-}
+  },
+  methods: {
+    navigateToHome: function() {
+      this.$router.push("/");
+    }
+  }
+};
 </script>
