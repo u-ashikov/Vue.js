c4d57e02d8eb3316545493cbb549143bd5c7913c
u.ashikov <u.ashikov@abv.bg>
2020-01-19 14:17:17 +0200

    Setting up active navigation link.

--- routing/src/components/Header.vue
+++ routing/src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <ul class="nav nav-pills">
-            <router-link to="/">Home</router-link>
-            <router-link to="/user">User</router-link>
+            <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+            <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
         </ul>
     </div>
 </template>
--- routing/src/routes.js
+++ routing/src/routes.js
@@ -1,7 +1,7 @@
 import Home from './components/Home.vue'
-import UserStart from './components/user/UserStart.vue'
+import User from './components/user/User.vue'
 
 export const routes = [
     { path: '', component: Home },
-    { path: '/user', component: UserStart }
+    { path: '/user', component: User }
 ];
