80ab4ce9c2582cbefcde8dc7f9148443ecb208bc
u.ashikov <u.ashikov@abv.bg>
2020-01-25 23:35:41 +0200

    Created Home component.

---
+++ my-projects/bookly/src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="jumbotron my-3">
+    <h1 class="display-4">Hello, Welcome to my books application :)</h1>
+    <p class="lead">This is a simple web application written with Vue JS.</p>
+    <hr class="my-4" />
+    <p>You can list, create, edit and delete books.</p>
+    <router-link tag="a" class="btn btn-primary btn-lg" to="/books/all">See Books</router-link>
+  </div>
+</template>
