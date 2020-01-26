063ef9e60054dc2b1d14c198687419f21598529a
u.ashikov <u.ashikov@abv.bg>
2020-01-26 20:39:24 +0200

    Added component for creating books.

---
+++ my-projects/bookly/src/components/books/CreateBook.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1 class="text-center">Create New Book</h1>
+    <hr />
+
+    <form class="col-md-6 mx-auto" method="post">
+        <div class="form-group">
+            <label for="title" class="h6">Title</label>
+            <input name="title" class="form-control"  v-model="title"/>
+        </div>
+
+        <div class="form-group">
+            <label for="author" class="h6">Author</label>
+            <input name="author" class="form-control" v-model="author" />
+        </div>
+
+        <input v-on:click.prevent="createBook" type="submit" class="btn btn-success" value="Create Book"/>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data: function () {
+        return {
+            author: '',
+            title: ''
+        }
+    },
+    methods: {
+        createBook: function () {
+            var self = this;
+
+            axios.post('https://books-b6a94.firebaseio.com/Books.json', { author: this.author, title: this.title})
+            .then(function (response) {
+                if (response && response.status == 200) {
+                    self.$router.push('/books/all');
+                }
+            });
+
+            this.title = '';
+            this.author = '';
+        }
+    }
+}
+</script>
