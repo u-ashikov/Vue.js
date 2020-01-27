2f7e4de8ec9546c81a1c56fb9f2da3b3a8604b3a
u.ashikov <u.ashikov@abv.bg>
2020-01-27 23:08:26 +0200

    Created dal object for querying the database.

---
+++ my-projects/bookly/src/dal/books.js
@@ -0,0 +1,23 @@
+import axios from '../../node_modules/axios'
+
+var axiosInstance = axios.create({
+    baseURL: 'https://books-b6a94.firebaseio.com'
+});
+
+function create(title, author) {
+    return axiosInstance.post('/Books.json', { author: author, title: title});
+}
+
+function getAll() {
+    return axiosInstance.get('/Books.json');
+}
+
+function deleteById(id) {
+    return axiosInstance.delete('/Books/' + id + '.json');
+}
+
+export default {
+    create,
+    getAll,
+    deleteById
+}
