f9200ed1ce14e02c69486b9bcc516d80bece2f4b
Yulian Ashikov <u.ashikov@abv.bg>
2020-02-02 21:49:35 +0200

    Created login and register components.

---
+++ my-projects/bookly/src/components/users/Login.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1 class="text-center">Login</h1>
+        <hr>
+        <form class="col-md-4 mx-auto" method="post" v-on:submit.prevent="loginUser">
+        <div class="form-group">
+            <label for="email" class="h6">Email</label>
+            <input id="email" type="email" class="form-control" v-model="email"/>
+        </div>
+
+        <div class="form-group">
+            <label for="password" class="h6">Password</label>
+            <input id="password" type="password" class="form-control" v-model="password"/>
+        </div>
+
+        <input type="submit" class="btn btn-success" value="Login" />
+    </form>
+    </div>
+</template>
+
+<script>
+import users from '../../queries/users'
+import { store } from '../../store/store'
+
+export default {
+    data: function () {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        loginUser: function () {
+            var self = this;
+
+            store.dispatch('login', { email: this.email, password: this.password })
+                .then(function () {
+                    self.$router.push('/');
+                });
+        }
+    }
+}
+</script>
---
+++ my-projects/bookly/src/components/users/Register.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1 class="text-center">Register</h1>
+    <hr />
+    <form class="col-md-4 mx-auto" method="post" v-on:submit.prevent="registerUser">
+      <div class="form-group">
+        <label for="username" class="h6">Username</label>
+        <input id="username" type="text" class="form-control" v-model="username" />
+      </div>
+
+      <div class="form-group">
+        <label for="email" class="h6">Email</label>
+        <input id="email" type="email" class="form-control" v-model="email" />
+      </div>
+
+      <div class="form-group">
+        <label for="password" class="h6">Password</label>
+        <input id="password" type="password" class="form-control" v-model="password" />
+      </div>
+
+      <input type="submit" class="btn btn-success" value="Register" />
+    </form>
+  </div>
+</template>
+
+<script>
+import users from "../../queries/users";
+
+export default {
+  data: function() {
+    return {
+      username: "",
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    registerUser: function() {
+        var self = this;
+
+        this.$store.dispatch('register', { username: this.username, email: this.email, password: this.password})
+          .then(function () {
+            self.$router.push('/');
+          });
+    }
+  }
+};
+</script>
