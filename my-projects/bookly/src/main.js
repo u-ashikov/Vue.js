0626c5854a607d67c685072b1860579a3123011a
Yulian Ashikov <u.ashikov@abv.bg>
2020-02-02 21:50:45 +0200

    Created store for state management. Added actions and mutations for login and register user.

--- my-projects/bookly/src/main.js
+++ my-projects/bookly/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import { router } from './routing/router.js'
+import { store } from './store/store.js'
 
 Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
+  store,
   router,
   render: h => h(App)
 })
---
+++ my-projects/bookly/src/store/store.js
@@ -0,0 +1,103 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+import users from '../queries/users'
+import router from 'vue-router'
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    state: {
+        idToken: null,
+        userId: null,
+        user: null
+    },
+    mutations: {
+        login: function (state, userData) {
+            state.idToken = userData.idToken;
+            state.userId = userData.userId;
+        },
+        register: function (state, userData) {
+            state.idToken = userData.idToken;
+            state.userId = userData.userId;
+        },
+        storeUser: function (state, user) {
+            state.user = user;
+        }
+    },
+    actions: {
+        login: function (context, user) {
+            users.login(user.email, user.password)
+                .then(function (response) {
+                    if (response && response.status == 200) {
+                        context.commit('login', {
+                            idToken: response.data.idToken,
+                            userId: response.data.localId
+                        });
+
+                        context.dispatch('fetchUser');
+                    }
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
+        },
+        register: function (context, user) {
+            users
+                .register(user.email, user.password)
+                .then(function (response) {
+                    if (response && response.status == 200) {
+                        context.commit('register', {
+                            idToken: response.data.idToken,
+                            userId: response.data.localId
+                        });
+
+                        context.dispatch('saveUser', user);
+                    }
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
+        },
+        saveUser: function (context, user) {
+            if (!context.state.idToken) {
+                return;
+            }
+
+            users
+                .save(user.username, user.email, user.password, context.state.idToken)
+                .then(function (response) {
+                    if (response && response.status == 200) {
+                        context.dispatch('fetchUser');
+                    }
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
+        },
+        fetchUser: function (context) {
+            if (!context.state.idToken) {
+                return;
+            }
+
+            users.getById(context.state.idToken)
+                .then(function (response) {
+                    if (response && response.status == 200) {
+                        var user = response.data.users[0];
+
+                        context.commit('storeUser', user);
+                    }
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
+        }
+    },
+    getters: {
+        username: function (state) {
+            return state.user ? state.user.email : null;
+        },
+        idToken: function (state) {
+            return state.idToken;
+        }
+    }
+});
