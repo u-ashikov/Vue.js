9dde984fc33fe3d19e918c8876941498657d4554
Yulian Ashikov <u.ashikov@abv.bg>
2020-02-03 20:58:47 +0200

    Display logged in user's username.

--- my-projects/bookly/src/components/Home.vue
+++ my-projects/bookly/src/components/Home.vue
@@ -5,5 +5,18 @@
     <hr class="my-4" />
     <p>You can list, create, edit and delete books.</p>
     <router-link tag="a" class="btn btn-primary btn-lg" to="/books/all">See Books</router-link>
+    <h3>{{ username }}</h3>
   </div>
 </template>
+
+<script>
+import { store } from '../store/store'
+
+export default {
+    computed: {
+      username: function () {
+        return store.getters.username?  store.getters.username : "Not logged in!";
+      }
+    }
+}
+</script>
