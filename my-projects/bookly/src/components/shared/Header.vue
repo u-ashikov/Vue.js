ce0a0aaec2126b13639184b88c6a1c955c3272f6
Yulian Ashikov <u.ashikov@abv.bg>
2020-02-04 21:15:01 +0200

    Refactored the nav bar, moved login, register and logout to the right. Included bootstrap scripts.

--- my-projects/bookly/src/components/shared/Header.vue
+++ my-projects/bookly/src/components/shared/Header.vue
@@ -1,27 +1,33 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <router-link to="/" class="navbar-brand" tag="a">BookLy</router-link>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNavAltMarkup"
-      aria-controls="navbarNavAltMarkup"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
+     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <router-link tag="a" class="nav-item nav-link" to="/">Home</router-link>
-        <router-link v-if="isAuthenticated" tag="a" class="nav-item nav-link" to="/books/all">Books</router-link>
-        <router-link v-if="isAuthenticated" tag="a" class="nav-item nav-link" to="/books/create">Create New</router-link>
-        <router-link v-if="!isAuthenticated" tag="a" class="nav-item nav-link" to="/users/register">Register</router-link>
-        <router-link v-if="!isAuthenticated" tag="a" class="nav-item nav-link" to="/users/login">Login</router-link>
-        <a v-if="isAuthenticated" href="/" class="nav-item nav-link" v-on:click="onLogout">Logout</a>
-      </div>
-    </div>
+     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+       <ul class="navbar-nav mr-auto">
+         <li class="nav-item">
+           <router-link tag="a" class="nav-link" to="/">Home</router-link>
+         </li>
+         <li class="nav-item">
+           <router-link v-if="isAuthenticated" tag="a" class="nav-link" to="/books/all">Books</router-link>
+         </li>
+         <li class="nav-item">
+           <router-link v-if="isAuthenticated" tag="a" class="nav-link" to="/books/create">Create New</router-link>
+         </li>
+       </ul>
+       <ul class="navbar-nav ml-auto">
+         <li class="nav-item">
+           <router-link v-if="!isAuthenticated" tag="a" class="nav-link" to="/users/login">Login</router-link>
+         </li>
+         <li class="nav-item">
+           <router-link v-if="!isAuthenticated" tag="a" class="nav-link" to="/users/register">Register</router-link>
+         </li>
+         <li class="nav-item">
+           <a v-if="isAuthenticated" href="/" class="nav-item nav-link ml-auto" v-on:click="onLogout">Logout</a>
+         </li>
+       </ul>
+     </div>
   </nav>
 </template>
 
