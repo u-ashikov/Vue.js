db84290a17fab288870764d8e2c1f76d93319b18
Yulian Ashikov <u.ashikov@abv.bg>
2020-02-09 00:06:03 +0200

    Added validations for the register user form.

--- my-projects/bookly/src/components/users/Register.vue
+++ my-projects/bookly/src/components/users/Register.vue
@@ -5,17 +5,24 @@
     <form class="col-md-4 mx-auto" method="post" v-on:submit.prevent="registerUser">
       <div class="form-group">
         <label for="username" class="h6">Username</label>
-        <input id="username" type="text" class="form-control" v-model="username" />
+        <input id="username" type="text" class="form-control" v-bind:class="{ invalid: $v.username.$error }" v-on:blur="$v.username.$touch()" v-model="username" />
+        <p class="text-danger" v-if="$v.username.$error && !$v.username.required">The field username is required.</p>
+        <p class="text-danger" v-if="$v.username.$error && !$v.username.unique">The username is already taken.</p>
       </div>
 
       <div class="form-group">
         <label for="email" class="h6">Email</label>
-        <input id="email" type="email" class="form-control" v-model="email" />
+        <input id="email" type="email" class="form-control" v-bind:class="{ invalid: $v.email.$error }" v-on:blur="$v.email.$touch()" v-model="email" />
+        <p class="text-danger" v-if="$v.email.$error && !$v.email.required">The email field is required.</p>
+        <p class="text-danger" v-if="!$v.email.email">Please enter a valid email address.</p>
+        <p class="text-danger" v-if="$v.email.$error && !$v.email.unique">The email is already taken.</p>
       </div>
 
       <div class="form-group">
         <label for="password" class="h6">Password</label>
-        <input id="password" type="password" class="form-control" v-model="password" />
+        <input id="password" type="password" class="form-control" v-bind:class="{ invalid: $v.password.$error }" v-on:blur="$v.password.$touch()" v-model="password" />
+        <p class="text-danger" v-if="$v.password.$error && !$v.password.required">The password field is required.</p>
+        <p class="text-danger" v-if="!$v.password.minLength">The password must be at least {{ $v.password.$params.minLength.min }} symbols length.</p>
       </div>
 
       <input type="submit" class="btn btn-success" value="Register" />
@@ -28,7 +35,8 @@
 </template>
 
 <script>
-import users from "../../queries/users";
+import users from "../../queries/users"
+import { required, email, minLength } from 'vuelidate/lib/validators'
 
 export default {
   data: function() {
@@ -38,9 +46,54 @@ export default {
       password: ""
     };
   },
+  validations: {
+    username: {
+      required,
+      unique: function (val) {
+        if (val === '') {
+          return true;
+        }
+
+        return users.getByUsername(val)
+                .then(function (response) {
+                  if (response && response.status == 200) {
+                    return Object.keys(response.data).length == 0;
+                  }
+
+                  return true;
+                });
+      }
+    },
+    email: {
+      required,
+      email,
+      unique: function (val) {
+        if (val === '') {
+          return true;
+        }
+
+        return users.getByEmail(val)
+                .then(function (response) {
+                  if (response && response.status == 200) {
+                    return Object.keys(response.data).length == 0;
+                  }
+
+                  return true;
+                });
+      }
+    },
+    password: {
+      required,
+      minLength: minLength(6)
+    }
+  },
   methods: {
     registerUser: function() {
         var self = this;
+
+        if (this.$v.$invalid) {
+          return;
+        }
 
         this.$store.dispatch('register', { username: this.username, email: this.email, password: this.password})
           .then(function () {
