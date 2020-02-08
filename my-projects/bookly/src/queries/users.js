606d0b92ec46b172dd801c38d7b060816f10057d
Yulian Ashikov <u.ashikov@abv.bg>
2020-02-09 00:05:35 +0200

    Added queries for getting user by email and username.

--- my-projects/bookly/src/queries/users.js
+++ my-projects/bookly/src/queries/users.js
@@ -20,9 +20,19 @@ function getById(idToken) {
     return axiosInstance.post('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBYveRqdiAYd1CjrtvNpXblNnqSZDEj5qE', { idToken, returnSecureToken: true });
 }
 
+function getByUsername(username) {
+    return axiosInstance.get('https://books-b6a94.firebaseio.com/Users.json?orderBy="username"&equalTo="' + username + '"');
+}
+
+function getByEmail(email) {
+    return axiosInstance.get('https://books-b6a94.firebaseio.com/Users.json?orderBy="email"&equalTo="' + email + '"');
+}
+
 export default {
     register,
     login,
     save,
-    getById
+    getById,
+    getByUsername,
+    getByEmail
 }
